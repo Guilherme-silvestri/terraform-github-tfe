1ea417af5e8350cfefc09a7da5b9121709b4ede5
Guilherme-silvestri <64138274+Guilherme-silvestri@users.noreply.github.com>
2020-08-05 10:12:36 -0300

    Update work.tf

--- work.tf
+++ work.tf
@@ -1,3 +1,8 @@
+provider "tfe" {
+  hostname = "${var.hostname}"
+  token    = "${var.token}"
+}
+
 resource "tfe_workspace" "BCPWorkspace" {
   name         = "BCP_Workspce"
   organization = "BCP"
