7688fe8e3543fc814cbc92a99b66c9f58a64fc41
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 10:56:41 -0300

    Teste1056

---
+++ team.tf
@@ -0,0 +1,25 @@
+provider "tfe" {
+        token = "${var.token}"
+        hostname = "${var.hostname}"
+}
+
+
+resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA01" {
+  access       = "admin"
+  team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-01.id}"
+}
+
+resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA02" {
+  access       = "admin"
+  team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-02.id}"
+}
+
+resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA03" {
+  access       = "admin"
+  team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-03.id}"
+}
+
+
