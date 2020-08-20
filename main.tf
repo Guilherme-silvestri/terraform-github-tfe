2a66bc293d0bacf2a313c668349dbac6bd249a6b
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 10:48:41 -0300

    Teste1048

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
 provider "tfe" {
-        token = "BHtxcop03D18cQ.atlasv1.JPvfyijDRhTNnxpGRE12SFRtNSTCFw0zwxBbytewhcGZ8nei1rngA1duf2pfkNWyHTo"
-        hostname = "terraform.neteas.local"
+        token = "${var.token}"
+        hostname = "${var.hotname}"
 }
 
 resource "tfe_workspace" "DESA-01" {
   name         = "DESA-01"
-  organization = "neteas"
+  organization = "${var.organization}"
   auto_apply = true
 }
 
 resource "tfe_workspace" "DESA-02" {
   name         = "DESA-02"
-  organization = "neteas"
+  organization = "${var.organization}"
   auto_apply = true
 }
 
 resource "tfe_workspace" "DESA-03" {
   name         = "DESA-03"
-  organization = "neteas"
+  organization = "${var.organization}"
   auto_apply = true
 }
 
 resource "tfe_team" "HTFE_SOPORTE_PROD" {
   name         = "HTFE_SOPORTE_PROD"
-  organization = "neteas"
+  organization = "${var.organization}"
 }
 
 resource "tfe_team" "HTFE_SECURITY_PROD" {
   name         = "HTFE_SECURITY_PROD"
-  organization = "neteas"
+  organization = "${var.organization}"
 }
 
 resource "tfe_team" "HTFE_DEVELOPER_APP1_PROD" {
   name         = "HTFE_DEVELOPER_APP1_PROD"
-  organization = "neteas"
+  organization = "${var.organization}"
 }
 
 resource "tfe_team" "HTFE_LT_APP1_PROD" {
   name         = "HTFE_LT_APP1_PROD"
-  organization = "neteas"
+  organization = "${var.organization}"
 }
 
 
