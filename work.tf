7a875874ef892c4c86ef342ee7c25825f04a4170
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 10:15:34 -0300

    Teste1015

--- work.tf
+++
@@ -1,9 +0,0 @@
-provider "tfe" {
-  hostname = "terraform.netbr.com.br"
-  token    = "SxmzFMe0SlzpFQ.atlasv1.NMF4RNvWvch08LAMML0yyMrBtdxqtqbIvFByDzYXh2HKvnFEWhIGbr2LHT9YRjJCdzU"
-}
-
-resource "tfe_workspace" "BCPWorkspace" {
-  name         = "BCP_Workspce"
-  organization = "BCP"
-}
