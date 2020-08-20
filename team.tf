979be8ead6b243c2d9e003409eb8f29c6db7e328
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 11:16:44 -0300

    Teste1116

--- team.tf
+++ team.tf
@@ -1,7 +1,13 @@
 resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA01" {
-  access       = "admin"
   team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
   workspace_id = "${tfe_workspace.DESA-01.id}"
+    permissions {
+      state_versions = "read"
+      variables = "write"
+      runs = "apply"
+      sentinel_mocks = "read"
+      workspace_locking = true
+  }
 }
 
 resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA02" {
@@ -17,9 +23,86 @@ resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA02" {
 }
 
 resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA03" {
-  access       = "admin"
   team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
   workspace_id = "${tfe_workspace.DESA-03.id}"
+    permissions {
+      state_versions = "read"
+      variables = "write"
+      runs = "apply"
+      sentinel_mocks = "read"
+      workspace_locking = true
+  }
 }
 
+resource "tfe_team_access" "HTFE_DEVELOPER_APP1_PROD-DESA01" {
+  team_id      = "${tfe_team.HTFE_DEVELOPER_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-01.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "plan"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
+
+resource "tfe_team_access" "HTFE_DEVELOPER_APP1_PROD-DESA02" {
+  team_id      = "${tfe_team.HTFE_DEVELOPER_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-02.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "plan"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
+
+resource "tfe_team_access" "HTFE_DEVELOPER_APP1_PROD-DESA03" {
+  team_id      = "${tfe_team.HTFE_DEVELOPER_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-03.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "plan"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
+
+resource "tfe_team_access" "HTFE_LT_APP1_PROD-DESA01" {
+  team_id      = "${tfe_team.HTFE_LT_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-01.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "apply"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
+
+resource "tfe_team_access" "HTFE_LT_APP1_PROD-DESA02" {
+  team_id      = "${tfe_team.HTFE_LT_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-02.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "apply"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
+
+resource "tfe_team_access" "HTFE_LT_APP1_PROD-DESA03" {
+  team_id      = "${tfe_team.HTFE_LT_APP1_PROD.id}"
+  workspace_id = "${tfe_workspace.DESA-03.id}"
+    permissions {
+      state_versions = "none"
+      variables = "read"
+      runs = "apply"
+      sentinel_mocks = "none"
+      workspace_locking = false
+  }
+}
 
