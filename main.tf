24c622376d382f2f789fc764a49fabe2578754d4
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 11:39:47 -0300

    Teste1139

--- main.tf
+++ main.tf
@@ -41,8 +41,4 @@ resource "tfe_team" "HTFE_LT_APP1_PROD" {
   organization = "${var.organization}"
 }
 
-resource "tfe_team" "HTFE_SECURITY_PROD" {
-  name         = "HTFE_SECURITY_PROD"
-  organization = "${var.organization}"
-}
 
