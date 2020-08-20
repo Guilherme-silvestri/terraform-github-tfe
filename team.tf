34530075b36665652697b5233100948c1a0c5dbf
Guilherme Silvestri <guilherme.silvestri@netbr.com.br>
2020-08-20 11:08:34 -0300

    Teste1108

--- team.tf
+++ team.tf
@@ -5,7 +5,6 @@ resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA01" {
 }
 
 resource "tfe_team_access" "HTFE_SOPORTE_PROD-DESA02" {
-  access       = "admin"
   team_id      = "${tfe_team.HTFE_SOPORTE_PROD.id}"
   workspace_id = "${tfe_workspace.DESA-02.id}"
   permissions {
